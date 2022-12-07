b49c20e9ae420432ce178d8b01a4166e5520316d
LuisSempre <luis.silva@viannasempre.com.br>
2022-12-07 20:23:46 -0300

    Create notes..jsx

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,16 +1,19 @@
-import { Link } from "@remix-run/react";
+import { Link } from '@remix-run/react';
 
+import homeStyles from '~/styles/home.css';
 
 export default function Index() {
   return (
-    <>
-    <main className='flex flex-col justify-center items-center h-full w-full max-w-4xl mx-auto'>
-        <h1>A better way of keeping track of your notes</h1>
-        <p>Try again!</p>
-        <button className='rounded-full bg-black text-white px-4 py-2'>
-          <Link to='/notes'>Try now!</Link>
-        </button>
-      </main>
-    </>
+    <main id="content">
+      <h1>A better way of keeping track of your notes</h1>
+      <p>Try our early beta and never loose track of your notes again!</p>
+      <p id="cta">
+        <Link to="/notes">Try Now!</Link>
+      </p>
+    </main>
   );
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: homeStyles }];
 }
